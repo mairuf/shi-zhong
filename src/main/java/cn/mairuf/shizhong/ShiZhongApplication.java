ef5c880542000adf0e99c4dfa6fc5d0d22c06be4
mairuf <mairuf@foxmail.com>
2025-09-25 15:07:09 +0800

    init: 初始化项目，引入必要依赖，项目可正常启动

---
+++ src/main/java/cn/mairuf/shizhong/ShiZhongApplication.java
@@ -0,0 +1,14 @@
+package cn.mairuf.shizhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiZhongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiZhongApplication.class, args);
+        System.out.println("时中 - 启动成功");
+    }
+
+}
---
+++ src/test/java/cn/mairuf/shizhong/ShiZhongApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mairuf.shizhong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiZhongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
